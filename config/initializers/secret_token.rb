b8f706a88361de18376430221ac5a3cc39f8d0e0
Hamilton <hamilton_etsii@hotmail.es>
2012-12-04 13:04:29 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TIO::Application.config.secret_token = 'fab4dbc8c3d96f5639c3421bcf2f13afa7a85aba9b076259736d63e526d923cc463aeba018d021ef0181b857093944aadba2ca11cdb55b29422b58e38a01d4f9'
